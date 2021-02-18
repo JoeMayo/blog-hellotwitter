b92b58d39db8e7be1d9cb0e9ac3a0b81f61a0809
Joe Mayo <jmayo@mayosoftware.com>
2021-02-17 20:15:38 -0800

    Initial Check-In

---
+++ HelloTwitter/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using LinqToTwitter;
+using LinqToTwitter.OAuth;
+
+namespace HelloTwitter
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            TwitterContext twitterCtx = await GetTwitterContext();
+
+            Console.WriteLine("Tweeting...");
+
+            await twitterCtx.TweetAsync(
+                "just setting up my #LinqToTwitter by @JoeMayo: " +
+                "\n\n#dotnet");
+
+            Console.WriteLine("Tweet Sent!");
+        }
+
+        static async Task<TwitterContext> GetTwitterContext()
+        {
+            var auth = new PinAuthorizer()
+            {
+                CredentialStore = new InMemoryCredentialStore
+                {
+                    ConsumerKey = "",
+                    ConsumerSecret = ""
+                },
+                GoToTwitterAuthorization = pageLink =>
+                {
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = pageLink,
+                        UseShellExecute = true
+                    };
+                    Process.Start(psi);
+                },
+                GetPin = () =>
+                {
+                    Console.WriteLine(
+                        "\nAfter authorizing this application, Twitter " +
+                        "will give you a 7-digit PIN Number.\n");
+                    Console.Write("Enter the PIN number here: ");
+                    return Console.ReadLine() ?? string.Empty;
+                }
+            };
+
+            await auth.AuthorizeAsync();
+
+            var twitterCtx = new TwitterContext(auth);
+            return twitterCtx;
+        }
+    }
+}
